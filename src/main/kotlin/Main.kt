ab2758e7232dddd5fa06badbf3e76a1737c56a6e
Muchamad Andriyanto <mandri8@gmail.com>
2023-03-06 22:25:16 +0700

    Proses Input

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,28 @@
-fun main() {
+fun main(){
+
+    println("-------------------------------------------------------")
+    println("INPUT TRANSAKSI PENJUALAN BAJU")
+    println("-------------------------------------------------------")
+    print("Nama Pembeli : ")
+    val namaPembeli:String = readLine().toString()
+    print("Baju : ")
+    val baju:String = readLine().toString()
+    print("Jumlah : ")
+    val jumlah:Int = readLine()?.toInt() ?: 0
+    print("Harga : ")
+    val harga:Int = readLine()?.toInt() ?: 0
+    print("Uang Bayar : ")
+    val uangBayar:Int = readLine()?.toInt() ?: 0
+    val kembali:Int = uangBayar-(harga*jumlah)
+
+
+    val pembeli = Penjualan(namaPembeli, baju, jumlah, harga, uangBayar, kembali)
+
+    output(pembeli)
+}
+
+fun output(pembeli: Penjualan) {
+
 
 }
 
