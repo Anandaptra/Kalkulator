6ee33840757eca38720454e5154f9e7b7a18870a
DwikiCahyo <70952085+DwikiCahyo@users.noreply.github.com>
2023-03-06 21:43:21 +0700

    commit method in class penjualan

--- src/main/kotlin/Penjualan.kt
+++ src/main/kotlin/Penjualan.kt
@@ -25,4 +25,25 @@ class Penjualan(
         fun getTotal(){
             println("Total : Rp $total")
         }
+
+        fun getBayar(){
+            println("Bayar: Rp $uangBayar")
+        }
+
+        fun getKembalian(){
+            println("Kembali: Rp $kembali")
+        }
+
+        fun getBonus(){
+            val hargaDiskon = total-50000
+            if(total in 150001..200000) {
+                println("Bonus : 1 buah Gelang")
+            } else if (total in 200001..300000){
+                println("Bonus : 1 buah Gantungan Kunci")
+            } else if (total > 300000){
+                println("Bonus : Diskon 50000 , Harga Akhir : $hargaDiskon)")
+            } else {
+                println("Bonus : 0")
+            }
+        }
 }
